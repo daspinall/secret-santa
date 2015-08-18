ed8fecd255cba91097616c93586e6f2ec00bfeb0
David Aspinall <david.w.aspinall@gmail.com>
2015-08-17 23:19:30 -0700

    secret santa algorithm

--- assign.py
+++ assign.py
@@ -22,18 +22,28 @@ class Group:
 		for i in range(self.size):
 			self.people[i].number = i
 
+	#Returns a dictionary of numbers assigned to other numbers
+	def secret_santa(self, num_people):
+		
+		assignments = {}
+		current = 0
+		assignment = randint(1, num_people - 1)
+
+		while len(assignments.keys()) < num_people:
+
+			assignments[current] = assignment
+
+			current = assignment
+
+			while assignments.has_key(assignment):
+				assignment = randint(0, num_people - 1)
+
+		return assignments
+
+
 group = Group()
 group.add_person("Sam", "sfarid93@gmail.com")
 group.add_person("David", "david.w.aspinall@mgmail.com")
 group.assign_numbers()
 
 
-#Returns a dictionary of numbers assigned to other numbers
-def secret_santa(num_people):
-	
-	assignments = {}
-	used_people = []
-
-	while len(assignments.
-
-	return assignments
