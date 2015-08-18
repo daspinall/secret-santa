4ec2abca20bf20c6804321127f5977bca16b2431
David Aspinall <david.w.aspinall@gmail.com>
2015-08-17 22:29:43 -0700

    fixed conflict

--- assign.py
+++ assign.py
@@ -1,14 +1,11 @@
 # TODO create unique random list of 8 numbers with only one cycle
 
-<<<<<<< HEAD
 from random import randint
 
 def secret_santa(people):
 	
-=======
 peopleToEmails = {
 	"sfarid93@gmail.com":"Sam"
 	"david.w.aspinall@gmail.com":"David"
 }
 
->>>>>>> 25dc85a464d485764b77f30b8313e45c855728ea
